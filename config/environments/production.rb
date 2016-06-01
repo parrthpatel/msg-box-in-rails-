02669f06773b477ae82eb4f44cb36e55ff36254a
Parth Patel <parth1634@gmail.com>
2016-06-01 10:05:14 +0530

    Production chages

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,5 +74,16 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 '587',
+  domain:               'gmail.com',
+  :user_name          => 'demo.botree@gmail.com',
+  :password           => 'India@123!',
+  authentication:       'login',
+  enable_starttls_auto: true  
+  }
 end
