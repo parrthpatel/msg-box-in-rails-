dd74193b4c04d6a576fbf2344bc7f76e55825a59
Parth Patel <parth1634@gmail.com>
2016-06-01 10:26:31 +0530

    sendgride add

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,19 +72,17 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
-  # Do not dump schema after migrations.
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'https://chatwale.herokuapp.com' }
   
-  config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 '587',
-  domain:               'gmail.com',
-  :user_name          => 'theskygarg@gmail.com',
-  :password           => 'adp410@42501',
-  authentication:       'login',
-  enable_starttls_auto: true  
-  }
+ ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
 end
